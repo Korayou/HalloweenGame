37bdc139fdcaa3d3fb19dc4b2e6a6ec6a72c2b48
Kayyissa <kayyissa@haissous.name>
2023-10-02 10:35:12 +0200

    initial commit (c fo)

---
+++ Assets/GraveyardProps/Scripts/rotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+
+    private float y = 0.0f;
+	[SerializeField] private float rotation = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.localRotation = Quaternion.Euler(0, y, 0);
+        y += rotation;
+        
+
+    }
+}
